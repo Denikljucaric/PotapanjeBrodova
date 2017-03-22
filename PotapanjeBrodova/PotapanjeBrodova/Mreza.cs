4ad925c53a3d6d9f7b609f3c56010cdac10f07ba
IceDrop <IceDrop@DESKTOP-OOI8QBS>
2017-03-22 20:41:24 +0100

    dodali dodavanje polja u mrezu i testove

--- PotapanjeBrodova/PotapanjeBrodova/Mreza.cs
+++ PotapanjeBrodova/PotapanjeBrodova/Mreza.cs
@@ -8,6 +8,34 @@ namespace PotapanjeBrodova
 {
     public class Mreza
     {
-        
+        public Mreza(int redaka, int stupaca)
+        {
+            this.redaka = redaka;
+            this.stupaca = stupaca;
+            polje = new Polje[redaka, stupaca];
+            for (int r = 0; r < redaka; ++r)
+            {
+                for (int s = 0; s < stupaca; ++s)
+                    polje[r, s] = new Polje(r, s);
+            }
+        }
+        public IEnumerable<Polje> DajSlobodnaPolja() {
+            List<Polje> p = new List<Polje>();
+            for (int r = 0; r < redaka; ++r)
+            {
+                for (int s = 0; s < stupaca; ++s)
+                   if(polje[r,s]!=null) p.Add(polje[r, s]);
+            }
+            return p;
+        }
+        public void UkloniPolje(int redak, int stupac) {
+            polje[redak, stupac] = null;
+        }
+        public void UkloniPolje(Polje p)
+        {
+            polje[p.Redak, p.Stupac] = null;
+        }
+        private Polje[,] polje;
+        private int redaka, stupaca;
     }
 }
--- PotapanjeBrodova/UnitTestProject1/TestMreze.cs
+++ PotapanjeBrodova/UnitTestProject1/TestMreze.cs
@@ -1,14 +1,62 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-
+using PotapanjeBrodova;
+using System.Linq;
 namespace UnitTestProject1
 {
     [TestClass]
     public class TestMreze
     {
         [TestMethod]
-        public void TestMethod1()
+        public void mreža_DajSlobodnaPoljaVracaSvaPoljaZaPočetnuMrežu()
         {
+            Mreza m1 = new Mreza(5, 5);
+            Assert.AreEqual(25, m1.DajSlobodnaPolja().Count());
+
+        }
+        [TestMethod]
+        public void mreža_DajSlobodnaPoljaVraca24PoljaZaMrežu5x5NakonJednogUklonjenogPoljaZadanogRetkomIstupcem()
+        {
+            Mreza m1 = new Mreza(5, 5);
+            m1.UkloniPolje(new Polje(1, 1));
+            Assert.AreEqual(24, m1.DajSlobodnaPolja().Count());
+            Assert.IsFalse(m1.DajSlobodnaPolja().Contains(new Polje(1, 1)));
+        }
+        [TestMethod]
+        public void mreža_DajSlobodnaPoljaVraca24PoljaZaMrežu5x5NakonJednogUklonjenogPolja()
+        {
+            Mreza m1 = new Mreza(5, 5);
+            m1.UkloniPolje(1,1);
+            Assert.AreEqual(24, m1.DajSlobodnaPolja().Count());
+            Assert.IsFalse(m1.DajSlobodnaPolja().Contains(new Polje(1,1)));
+        }
+        [TestMethod]
+        public void mreža_DajSlobodnaPoljaVraca23PoljaZaMrežu5x5NakondvaUklonjenogPolja()
+        {
+            Mreza m1 = new Mreza(5, 5);
+            m1.UkloniPolje(1, 1);
+            m1.UkloniPolje(4, 4);
+            Assert.AreEqual(23, m1.DajSlobodnaPolja().Count());
+            Assert.IsFalse(m1.DajSlobodnaPolja().Contains(new Polje(1, 1)));
+            Assert.IsFalse(m1.DajSlobodnaPolja().Contains(new Polje(4, 4)));
+        }
+        [TestMethod]
+        public void mreža_UkloniPoljeBacaIznimkuZaNepostojeciRedak()
+        {
+            try
+            {
+                Mreza m1 = new Mreza(5, 5);
+
+                m1.UkloniPolje(6, 1);
+                Assert.Fail();
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Assert.IsTrue(true);
+            }
+            catch (Exception) {
+                Assert.Fail();
+            }
         }
     }
 }
