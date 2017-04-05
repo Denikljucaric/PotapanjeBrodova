970b0ef73ea0d93e1c5a67b02440d33a237233b3
dkljucaric <IceDrop@DESKTOP-OOI8QBS>
2017-04-05 21:10:01 +0200

    radiii

---
+++ PotapanjeBrodova/PotapanjeBrodova/TerminatorPolja.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PotapanjeBrodova
+{
+    public class TerminatorPolja
+    {
+        public TerminatorPolja(Mreža mreza) {
+            this.mreza = mreza;
+        }
+        public void UkloniPolja(IEnumerable<Polje> polja) {
+            //nesto
+           // mreza.UkloniPolje(r, s);
+        }
+        private Mreža mreza;
+    }
+}
