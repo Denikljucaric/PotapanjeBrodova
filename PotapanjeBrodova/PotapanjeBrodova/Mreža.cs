44161016c16057119ed2fd797ddd2274bade30ef
dkljucaric <IceDrop@DESKTOP-OOI8QBS>
2017-04-05 20:09:56 +0200

    Promjenjene metode

--- PotapanjeBrodova/PotapanjeBrodova/Mreža.cs
+++ PotapanjeBrodova/PotapanjeBrodova/Mreža.cs
@@ -43,53 +43,44 @@ namespace PotapanjeBrodova
             polja[p.Redak, p.Stupac] = null;
         }
 
+        IEnumerable<int> DajNizBrojeva(int maxVrijednost) {
+            for (int i = 0; i < maxVrijednost; ++i)yield return i;
+        }
+        private IEnumerable<IEnumerable<Polje>> DajNizoveSlobodnihPolja(int duljinaNiza, IEnumerable<int> vanjskiIndex, IEnumerable<int> nutarnjiIndex, Func<int, int, Polje> dohvatPolja) {
+            List<IEnumerable<Polje>> nizovi = new List<IEnumerable<Polje>>();
+            foreach (int i in vanjskiIndex)
+            {
+                List<Polje> tmp = new List<Polje>();
+                foreach (int j in nutarnjiIndex)
+                {
+                    Polje polje = dohvatPolja(i,j) ;
+                    if (polje != null) tmp.Add(polje);
+                    else tmp.Clear();
+                    if (tmp.Count == duljinaNiza)
+                    {
+                        nizovi.Add(new List<Polje>(tmp));
+                        tmp.RemoveAt(0);
+                    }
+                }
+            }
+            return nizovi;
+
+        }
+        public IEnumerable<IEnumerable<Polje>> DajNizoveSlobodnihPoljaUVertikalnomSmjeru(int duljinaNiza) {
+
+            return DajNizoveSlobodnihPolja(duljinaNiza, DajNizBrojeva(stupaca), DajNizBrojeva(redaka),(i, j) => polja[j, i]);
+        }
+        public IEnumerable<IEnumerable<Polje>> DajNizoveSlobodnihPoljaUHorizontalnomSmjeru(int duljinaNiza)
+        {
+            return DajNizoveSlobodnihPolja(duljinaNiza, DajNizBrojeva(redaka), DajNizBrojeva(stupaca),(i,j) => polja[i,j]);
+        }
+
         public IEnumerable<IEnumerable<Polje>> DajNizoveSlobodnihPolja(int duljinaNiza)
         {
             List<IEnumerable<Polje>> nizovi = new List<IEnumerable<Polje>>();
-            //horizontala
-            for (int r = 0; r < redaka; ++r)
-            {
-                List<Polje> tmp = new List<Polje>();
+            nizovi.AddRange(DajNizoveSlobodnihPoljaUHorizontalnomSmjeru(duljinaNiza));
+            nizovi.AddRange(DajNizoveSlobodnihPoljaUVertikalnomSmjeru(duljinaNiza));
 
-                for (int s = 0, i = 0; s < stupaca; ++s)
-                {
-                    if (polja[r, s] != null)
-                    {
-                        tmp.Add(polja[r, s]);
-                    }
-                    else
-                    {
-                        tmp.Clear();
-                    }
-                    if (tmp.Count == duljinaNiza)
-                    {
-                        nizovi.Add(new List<Polje>(tmp));
-                        tmp.RemoveAt(0);
-                    }
-                }
-            }
-            //vertikala
-            for (int s = 0; s < stupaca; ++s)
-            {
-                List<Polje> tmp = new List<Polje>();
-
-                for (int r = 0, i = 0; r < redaka; ++r)
-                {
-                    if (polja[r, s] != null)
-                    {
-                        tmp.Add(polja[r, s]);
-                    }
-                    else
-                    {
-                        tmp.Clear();
-                    }
-                    if (tmp.Count == duljinaNiza)
-                    {
-                        nizovi.Add(new List<Polje>(tmp));
-                        tmp.RemoveAt(0);
-                    }
-                }
-            }
 
             return nizovi;
         }
