cb70d52d28bd2a0b1899b8ed2d64eb3a89796d21
dkljucaric <IceDrop@DESKTOP-OOI8QBS>
2017-04-05 20:34:22 +0200

    Dodali metodu Brod

---
+++ PotapanjeBrodova/PotapanjeBrodova/Brod.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PotapanjeBrodova
+{
+   public class Brod
+    {
+        public Brod(IEnumerable<Polje> polja) {
+            Polja = polja;
+        }
+        public readonly IEnumerable<Polje> Polja;
+    }
+}
+    
