be47756a8ec73f55283517f63f60eac4fe7ff00d
dkljucaric <IceDrop@DESKTOP-OOI8QBS>
2017-05-03 18:36:12 +0200

    da

---
+++ PotapanjeBrodova/PotapanjeBrodova/Brodograditelj.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PotapanjeBrodova
+{
+    public class Brodograditelj
+    {
+        public Flota SloziFlotu(Mreža mreza, IEnumerable<int> duljineBrodova) {
+            Flota flota = new Flota();
+            TerminatorPolja tp = new TerminatorPolja(mreza);
+            foreach (int i in duljineBrodova) {
+                var nizovi = mreza.DajNizoveSlobodnihPolja(i);
+                int index = slucajni.Next(nizovi.Count());
+                var niz = nizovi.ElementAt(index);
+                flota.DodajBrod(niz);
+                tp.UkloniPolja(niz);
+            }
+            return flota;
+            //todo popraviti jer moze se desiti da nema vise slobodnih polja
+
+        }
+        Random slucajni = new Random();
+
+    }
+}
---
+++ PotapanjeBrodova/Test/TestBrodograditelja.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PotapanjeBrodova;
+namespace Test
+{
+    /// <summary>
+    /// Summary description for TestBrodograditelja
+    /// </summary>
+    [TestClass]
+    public class TestBrodograditelja
+    {
+        public TestBrodograditelja()
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+        }
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        //
+        // You can use the following additional attributes as you write your tests:
+        //
+        // Use ClassInitialize to run code before running the first test in the class
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // Use ClassCleanup to run code after all tests in a class have run
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        // Use TestInitialize to run code before running each test 
+        // [TestInitialize()]
+        // public void MyTestInitialize() { }
+        //
+        // Use TestCleanup to run code after each test has run
+        // [TestCleanup()]
+        // public void MyTestCleanup() { }
+        //
+        #endregion
+
+        [TestMethod]
+        public void Brodograditelj_SloziFlotuVracaFlotuBrodovimaZadaneDuljine()
+        {
+            Brodograditelj b = new Brodograditelj();
+            Mreža mreza = new Mreža(10, 10);
+            IEnumerable<int> duljineBrodova = new int[] { 5, 4, 4, 3, 3, 3, 2, 2, 2, 2 };
+            Flota f = b.SloziFlotu(mreza, duljineBrodova);
+            Assert.AreEqual(duljineBrodova.Count(), f.BrojBrodova);
+            //todo provjerit ako ima samo jedan duljine 5 2 dljine 4 ...
+        }
+    }
+}
