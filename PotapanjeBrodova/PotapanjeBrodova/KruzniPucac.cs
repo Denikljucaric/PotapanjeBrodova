4a122fa4143dd06cc17c198554a16c1400fae4f7
dkljucaric <IceDrop@DESKTOP-OOI8QBS>
2017-05-10 16:57:02 +0200

    kruzni pucac liniski pucac slucajni pucac

---
+++ PotapanjeBrodova/PotapanjeBrodova/KruzniPucac.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PotapanjeBrodova
+{
+    public class KruzniPucac : IPucac
+    {
+        public Polje Gadaj()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ObradiGadanje(RezultatGađanja rezultat)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ PotapanjeBrodova/PotapanjeBrodova/LiniskiPucac.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PotapanjeBrodova
+{
+    class LiniskiPucac : IPucac
+    {
+        public Polje Gadaj()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ObradiGadanje(RezultatGađanja rezultat)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
